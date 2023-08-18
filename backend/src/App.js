4f7caadfac04f08a96c7334321a63f564e35a28a
Patrick Boynton <jpboynton3@gmail.com>
2023-08-18 08:26:53 -0400

    Add middleware

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,11 +1,39 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import mongoSanitize from 'express-mongo-sanitize'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import fileUpload from 'express-fileupload'
 
 dotenv.config()
 
 const app = express()
 
 const PORT = process.env.PORT
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+if (process.env.NODE_ENV !== 'development') {
+	app.use(morgan('dev'))
+}
+
+app.use(helmet())
+
+app.use(mongoSanitize())
+
+app.use(cookieParser())
+
+app.use(compression())
+
+app.use(
+	fileUpload({
+		useTempFiles: true,
+	})
+)
 
 app.get('/', (req, res) => {
 	res.send('The API is running!')
