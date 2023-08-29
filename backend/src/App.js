36244287d6a503c7bd883cce794678e05a3a8f96
Patrick Boynton <jpboynton3@gmail.com>
2023-08-29 11:43:24 -0400

    Add authentication for the password

--- backend/src/App.js
+++ backend/src/App.js
@@ -8,24 +8,23 @@ import compression from 'compression'
 import fileUpload from 'express-fileupload'
 import createHttpError from 'http-errors'
 import routes from './routes/index.js'
-import logger from "./config/logger.js";
-import {connectDB} from "./config/connect.js";
+import logger from './config/logger.js'
+import { connectDB } from './config/connect.js'
 
 dotenv.config()
 
 const app = express()
 
-const {PORT, DATABASE_URL} = process.env
-
+const { PORT, DATABASE_URL } = process.env
 
 connectDB(DATABASE_URL)
 
 app.use(express.json())
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV !== 'development') {
-    app.use(morgan('dev'))
+	app.use(morgan('dev'))
 }
 
 app.use(helmet())
@@ -37,58 +36,57 @@ app.use(cookieParser())
 app.use(compression())
 
 app.use(
-    fileUpload({
-        useTempFiles: true,
-    })
+	fileUpload({
+		useTempFiles: true,
+	}),
 )
 
-app.get("/", (req, res) => {
-    res.send('API IS RUNNING!')
+app.get('/', (req, res) => {
+	res.send('API IS RUNNING!')
 })
 
+// http://localhost:8000/api
 app.use('/api', routes)
 
-
 app.use(async (req, res, next) => {
-    next(createHttpError.NotFound('Route found is not.'))
+	next(createHttpError.NotFound('Route found is not.'))
 })
 
 app.use(async (err, req, res, next) => {
-    res.status(err.status || 500)
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message
-        }
-    })
+	res.status(err.status || 500)
+	res.send({
+		error: {
+			status: err.status || 500,
+			message: err.message,
+		},
+	})
 })
 
 const server = app.listen(PORT, () => {
-    logger.info(`Go to http://localhost:${PORT}`)
-    const pid = process.pid
-    logger.info(`PID: ${pid}`)
+	logger.info(`Go to http://localhost:${PORT}`)
+	const pid = process.pid
+	logger.info(`PID: ${pid}`)
 })
 
 const exitHandler = () => {
-    if (server) {
-        logger.info('Server closed')
-        process.exit(1)
-    } else {
-        process.exit(1)
-    }
+	if (server) {
+		logger.info('Server closed')
+		process.exit(1)
+	} else {
+		process.exit(1)
+	}
 }
 
-const unexpectedErrorHandler = (error) => {
-    logger.error(error)
-    exitHandler()
+const unexpectedErrorHandler = error => {
+	logger.error(error)
+	exitHandler()
 }
 
-process.on("uncaughtException", unexpectedErrorHandler)
-process.on("unhandledRejection", unexpectedErrorHandler)
-process.on("SIGTERM", () => {
-    if (server) {
-        logger.info('Server is closed.')
-        process.exit(1)
-    }
+process.on('uncaughtException', unexpectedErrorHandler)
+process.on('unhandledRejection', unexpectedErrorHandler)
+process.on('SIGTERM', () => {
+	if (server) {
+		logger.info('Server is closed.')
+		process.exit(1)
+	}
 })
-
